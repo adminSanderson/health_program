abe81b9d330ae8ec86535e3dc76e42531adc8bc9
Roman Grin <grinromchik@gmail.com>
2022-06-17 11:31:03 +0300

    commit: создан интерфейс файла my_app

--- final_win.py
+++ final_win.py
@@ -1,11 +1,12 @@
 from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import (
-        QApplication, QWidget, 
-        QHBoxLayout, QVBoxLayout, QGridLayout, 
-        QGroupBox, QRadioButton,
-        QPushButton, QLabel, QListWidget, QLineEdit)
-        
+    QApplication, QWidget,
+    QHBoxLayout, QVBoxLayout, QGridLayout,
+    QGroupBox, QRadioButton,
+    QPushButton, QLabel, QListWidget, QLineEdit)
+
 from instr import *
+
 
 class FinalWin(QWidget):
     def __init__(self):
@@ -14,15 +15,24 @@ class FinalWin(QWidget):
 
         # создаём и настраиваем графические элелементы:
         self.initUI()
-        #устанавливает, как будет выглядеть окно (надпись, размер, место)
-        self.set_appear()      
+        # устанавливает, как будет выглядеть окно (надпись, размер, место)
+        self.set_appear()
         # старт:
         self.show()
 
     def initUI(self):
         ''' создает графические элементы '''
+
         pass
 
     ''' устанавливает, как будет выглядеть окно (надпись, размер, место) '''
+
     def set_appear(self):
-        pass
+        self.setWindowTitle(txt_title)
+        self.resize(win_width, win_height)
+        self.move(win_x, win_x)
+        
+
+app = QApplication([])
+mw = QApp
+
--- my_app.py
+++ my_app.py
@@ -1,14 +1,14 @@
 from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import (
-        QApplication, QWidget, 
-        QHBoxLayout, QVBoxLayout, 
-        QGroupBox, QRadioButton,
-        QPushButton, QLabel, QListWidget, QLineEdit)
+    QApplication, QWidget,
+    QHBoxLayout, QVBoxLayout,
+    QGroupBox, QRadioButton,
+    QPushButton, QLabel, QListWidget, QLineEdit)
 
 from instr import *
 from second_win import *
 
-       
+
 class MainWin(QWidget):
     def __init__(self):
         ''' окно, в котором располагается приветствие '''
@@ -16,17 +16,25 @@ class MainWin(QWidget):
 
         # создаём и настраиваем графические эелементы:
         self.initUI()
-        #устанавливает связи между элементами
+        # устанавливает связи между элементами
+
         self.connects()
-        #устанавливает, как будет выглядеть окно (надпись, размер, место)
+        # устанавливает, как будет выглядеть окно (надпись, размер, место)
         self.set_appear()
         # старт:
         self.show()
 
     def initUI(self):
         ''' создает графические элементы '''
-        pass
-   
+        self.btn_next = QPushButton(txt_next)
+        self.hello_next = QLabel(txt_hello)
+        self.instruction = QLabel(txt_instruction)
+
+        self.layout_vertical = QVBoxLayout()
+        self.layout_vertical.adWidget(self.btn_next, alignment=Qt.AligCenter)
+        self.layout_vertical.adWidget(self.hello_next, alignment=Qt.AligLeft)
+        self.layout_vertical.adWidget(self.instruction, alignment=Qt.AligLeft)
+
     def next_click(self):
         pass
 
@@ -34,10 +42,15 @@ class MainWin(QWidget):
         pass
 
     ''' устанавливает, как будет выглядеть окно (надпись, размер, место) '''
+
     def set_appear(self):
-        pass
+        self.setWindowTitle(txt_title)
+        self.resize(win_width, win_height)
+        self.move(win_x, win_y)
+
 
 app = QApplication([])
+
 mw = MainWin()
 
 app.exec_()
